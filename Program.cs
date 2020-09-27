26dfcb3148bf8928a432f826eb658f4384116c3f
marianaSilva-26 <mariana.silvadossantos.26@gmail.com>
2020-09-27 15:09:37 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Heron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double ladoA; 
+           double ladoB;
+           double ladoC;
+           double resultadoP;
+           double resultadoA;
+        
+           Console.Write("digite a medida ladoA");
+           ladoA = double.Parse(Console.ReadLine());
+
+           Console.Write("digite a medida ladoB");
+           ladoB = double.Parse(Console.ReadLine());
+
+           Console.Write("digite a medida ladoC");
+           ladoC = double.Parse(Console.ReadLine());
+           Console.WriteLine("\n");
+           resultadoP = ( ladoA + ladoB + ladoC ) /2;
+           resultadoA = ((resultadoP * (resultadoP - ladoA) * (resultadoP - ladoB) *(resultadoP - ladoC)));
+           
+           Console.WriteLine("semiperimetro = {0}" , resultadoP);
+           Console.WriteLine("area = {0} (cm²)" , resultadoA);
+           Console.Write("\n");
+
+           Console.WriteLine("pressione Enter para sair!");
+           Console.ReadKey();
+        }
+    }
+}
